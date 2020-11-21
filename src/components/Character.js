23234798e69fda899b400f34973f9272a08d2b96
bipin-shrestha <iambpn.shrestha@gmail.com>
2020-11-20 19:47:27 -0800

    Map and some work in Character component completed

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,11 @@
 // Write your Character component here
+import React from 'react';
+import Styled from 'styled-components';
+
+const Character = (props) => {
+const {character} = props;
+    return (
+    <div>{character.name}</div>
+    );
+}
+export default Character;
